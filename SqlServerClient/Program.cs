a4ddf662337f8eaac1a13831de6c8202795d69e2
YA <YA-androidapp@users.noreply.github.com>
2022-07-29 19:50:54 +0900

    Add models and READ function

--- SqlServerClient/Program.cs
+++ SqlServerClient/Program.cs
@@ -2,11 +2,21 @@
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using System.Text;
+using System.Collections.Generic;
+
+
+// Generate models
+// Scaffold-DbContext -connection "Data Source=..." -Provider "Microsoft.EntityFrameworkCore.SqlServer" -OutputDir Models
+
 
 namespace SqlServerClient
 {
     internal class Program
     {
+        private static string connectionString = "";
+
+        const string SQL_SELECT_PRODUCTS = "SELECT * FROM [SalesLT].[Product]";
+
         /// <summary>
         /// Contains the application's configuration settings. 
         /// </summary>
@@ -23,46 +33,85 @@ namespace SqlServerClient
                 .Build();
         }
 
-        static void Main(string[] args)
+        static void GetRecords()
         {
-            Program app = new();
-
-            var connectionString = app.Configuration.GetConnectionString("MyConn");
+            var models = new List<Models.Product>();
 
             try
             {
-                // SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-                // builder.DataSource = "<your_server>.database.windows.net";
-                // builder.UserID = "<your_username>";
-                // builder.Password = "<your_password>";
-                // builder.InitialCatalog = "<your_database>";
-
-                // using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
                 using (SqlConnection connection = new SqlConnection(connectionString))
+                using (SqlCommand command = new SqlCommand(SQL_SELECT_PRODUCTS, connection))
                 {
-                    Console.WriteLine("\nProducts:");
-                    Console.WriteLine("=========================================\n");
-
-                    String sql = "SELECT ProductID , Name FROM [SalesLT].[Product]";
-
-                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
                     {
-                        connection.Open();
-                        using (SqlDataReader reader = command.ExecuteReader())
+                        while (reader.Read())
                         {
-                            while (reader.Read())
+                            //Console.WriteLine("{0} {1}", reader.GetInt32(0), reader.GetString(1));
+                            var c = reader[nameof(Models.Product.SellEndDate)];
+                            var cc = c == DBNull.Value;
+
+                            models.Add(new Models.Product
                             {
-                                Console.WriteLine("{0} {1}", reader.GetInt32(0), reader.GetString(1));
-                            }
+                                ProductId = (int)reader[nameof(Models.Product.ProductId)],
+                                Name = reader[nameof(Models.Product.Name)].ToString(),
+                                ProductNumber = reader[nameof(Models.Product.ProductNumber)].ToString(),
+                                Color = reader[nameof(Models.Product.Color)].ToString(),
+                                StandardCost = (Decimal)reader[nameof(Models.Product.StandardCost)],
+                                ListPrice = (Decimal)reader[nameof(Models.Product.ListPrice)],
+                                Size = reader[nameof(Models.Product.Size)].ToString(),
+                                Weight = reader[nameof(Models.Product.Weight)] == DBNull.Value ? null : (Decimal)reader[nameof(Models.Product.Weight)],
+                                ProductCategoryId = reader[nameof(Models.Product.ProductCategoryId)] == DBNull.Value ? null : (int)reader[nameof(Models.Product.ProductCategoryId)],
+                                ProductModelId = reader[nameof(Models.Product.ProductModelId)] == DBNull.Value ? null : (int)reader[nameof(Models.Product.ProductModelId)],
+                                SellStartDate = (DateTime)reader[nameof(Models.Product.SellStartDate)],
+                                SellEndDate = reader[nameof(Models.Product.SellEndDate)] == DBNull.Value ? null : (DateTime)reader[nameof(Models.Product.SellEndDate)],
+                                DiscontinuedDate = reader[nameof(Models.Product.DiscontinuedDate)] == DBNull.Value ? null : (DateTime)reader[nameof(Models.Product.DiscontinuedDate)],
+                                ThumbNailPhoto = (byte[])reader[nameof(Models.Product.ThumbNailPhoto)],
+                                ThumbnailPhotoFileName = reader[nameof(Models.Product.ThumbnailPhotoFileName)].ToString(),
+                                Rowguid = (Guid)reader[nameof(Models.Product.Rowguid)],
+                                ModifiedDate = (DateTime)reader[nameof(Models.Product.ModifiedDate)]
+                            });
                         }
                     }
                 }
+                Console.WriteLine(models.Count);
             }
             catch (SqlException e)
             {
                 Console.WriteLine(e.ToString());
             }
-            Console.ReadLine();
+        }
+
+        static void GetRecordsPopulator()
+        {
+            var models = new List<Models.Product>();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                using (SqlCommand command = new SqlCommand(SQL_SELECT_PRODUCTS, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        models = new GenericPopulator<Models.Product>().CreateList(reader);
+                    }
+                }
+                Console.WriteLine(models.Count);
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program app = new();
+            connectionString = app.Configuration.GetConnectionString("MyConn");
+
+            GetRecords();
+            GetRecordsPopulator();
         }
     }
 }
---
+++ SqlServerClient/ReflectionPopulator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+
+namespace SqlServerClient
+{
+	public class GenericPopulator<T>
+	{
+		public virtual List<T> CreateList(SqlDataReader reader)
+		{
+			var results = new List<T>();
+			var properties = typeof(T).GetProperties();
+
+			while (reader.Read())
+			{
+				var item = Activator.CreateInstance<T>();
+				foreach (var property in typeof(T).GetProperties())
+				{
+					if (property.GetMethod.IsVirtual == false)
+					{
+						if (!reader.IsDBNull(reader.GetOrdinal(property.Name)))
+						{
+							Type convertTo = Nullable.GetUnderlyingType(property.PropertyType) ?? property.PropertyType;
+							property.SetValue(item, Convert.ChangeType(reader[property.Name], convertTo), null);
+						}
+					}
+				}
+				results.Add(item);
+			}
+			return results;
+		}
+	}
+}
