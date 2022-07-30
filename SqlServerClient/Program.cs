4cdd3d69ffe08dde333506755536be07ba83d269
YA <YA-androidapp@users.noreply.github.com>
2022-07-30 21:27:19 +0900

    Update Program.cs

--- SqlServerClient/Program.cs
+++ SqlServerClient/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
-using System.Text;
-using System.Collections.Generic;
 
 
 // Generate models
@@ -15,7 +12,6 @@ namespace SqlServerClient
     {
         private static string connectionString = "";
 
-        const string SQL_SELECT_PRODUCTS = "SELECT * FROM [SalesLT].[Product]";
 
         /// <summary>
         /// Contains the application's configuration settings. 
@@ -33,85 +29,16 @@ namespace SqlServerClient
                 .Build();
         }
 
-        static void GetRecords()
-        {
-            var models = new List<Models.Product>();
-
-            try
-            {
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                using (SqlCommand command = new SqlCommand(SQL_SELECT_PRODUCTS, connection))
-                {
-                    connection.Open();
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            //Console.WriteLine("{0} {1}", reader.GetInt32(0), reader.GetString(1));
-                            var c = reader[nameof(Models.Product.SellEndDate)];
-                            var cc = c == DBNull.Value;
-
-                            models.Add(new Models.Product
-                            {
-                                ProductId = (int)reader[nameof(Models.Product.ProductId)],
-                                Name = reader[nameof(Models.Product.Name)].ToString(),
-                                ProductNumber = reader[nameof(Models.Product.ProductNumber)].ToString(),
-                                Color = reader[nameof(Models.Product.Color)].ToString(),
-                                StandardCost = (Decimal)reader[nameof(Models.Product.StandardCost)],
-                                ListPrice = (Decimal)reader[nameof(Models.Product.ListPrice)],
-                                Size = reader[nameof(Models.Product.Size)].ToString(),
-                                Weight = reader[nameof(Models.Product.Weight)] == DBNull.Value ? null : (Decimal)reader[nameof(Models.Product.Weight)],
-                                ProductCategoryId = reader[nameof(Models.Product.ProductCategoryId)] == DBNull.Value ? null : (int)reader[nameof(Models.Product.ProductCategoryId)],
-                                ProductModelId = reader[nameof(Models.Product.ProductModelId)] == DBNull.Value ? null : (int)reader[nameof(Models.Product.ProductModelId)],
-                                SellStartDate = (DateTime)reader[nameof(Models.Product.SellStartDate)],
-                                SellEndDate = reader[nameof(Models.Product.SellEndDate)] == DBNull.Value ? null : (DateTime)reader[nameof(Models.Product.SellEndDate)],
-                                DiscontinuedDate = reader[nameof(Models.Product.DiscontinuedDate)] == DBNull.Value ? null : (DateTime)reader[nameof(Models.Product.DiscontinuedDate)],
-                                ThumbNailPhoto = (byte[])reader[nameof(Models.Product.ThumbNailPhoto)],
-                                ThumbnailPhotoFileName = reader[nameof(Models.Product.ThumbnailPhotoFileName)].ToString(),
-                                Rowguid = (Guid)reader[nameof(Models.Product.Rowguid)],
-                                ModifiedDate = (DateTime)reader[nameof(Models.Product.ModifiedDate)]
-                            });
-                        }
-                    }
-                }
-                Console.WriteLine(models.Count);
-            }
-            catch (SqlException e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-        }
-
-        static void GetRecordsPopulator()
-        {
-            var models = new List<Models.Product>();
-
-            try
-            {
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                using (SqlCommand command = new SqlCommand(SQL_SELECT_PRODUCTS, connection))
-                {
-                    connection.Open();
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        models = new GenericPopulator<Models.Product>().CreateList(reader);
-                    }
-                }
-                Console.WriteLine(models.Count);
-            }
-            catch (SqlException e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-        }
-
         static void Main(string[] args)
         {
             Program app = new();
             connectionString = app.Configuration.GetConnectionString("MyConn");
 
-            GetRecords();
-            GetRecordsPopulator();
+            RawSqlUtil.GetRecords(connectionString);
+            RawSqlUtil.GetRecordsPopulator(connectionString);
+            RawSqlUtil.AddRecord(connectionString, "Contoso", "Contoso", 12345, 12345, DateTime.Today, Guid.NewGuid(), DateTime.Today);
+            RawSqlUtil.ModifyRecordsByName(connectionString, "Contoso", "Contoso2");
+            RawSqlUtil.RemoveRecordsByName(connectionString, "Contoso");
         }
     }
 }
